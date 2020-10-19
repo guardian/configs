7b951d2bd2c5619c1e4b4f2fbee75247a6665235
Alex Sanders <a@snd.rs>
2020-10-19 16:57:00 +0100

    dont use eslint-comments recommended, be more specfic

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -8,15 +8,30 @@ module.exports = {
 		'eslint:recommended',
 		'plugin:import/errors',
 		'plugin:import/warnings',
-
-		// https://mysticatea.github.io/eslint-plugin-eslint-comments
-		'plugin:eslint-comments/recommended',
-
 		'prettier',
 	],
+	plugins: ['eslint-comments'],
 	rules: {
+		// require a `eslint-enable` comment for every `eslint-disable` comment
+		'eslint-comments/disable-enable-pair': [2, { allowWholeFile: true }],
+
+		// disallow duplicate `eslint-disable` comments
+		'eslint-comments/no-duplicate-disable': 2,
+
+		// disallow unused eslint-en/disable comments
+		// (make sure they're not left in after a fix)
+		'eslint-comments/no-unused-disable': 2,
+		'eslint-comments/no-unused-enable': 2,
+
 		// require an explanation if you disable eslint
-		'eslint-comments/require-description': 2,
+		'eslint-comments/require-description': [
+			2,
+			{ ignore: ['eslint-enable'] },
+		],
+
+		// this doesn't catch bugs or make things more readable
+		// and sometimes tools or 3rd parties require it
+		'no-underscore-dangle': 0,
 
 		'import/no-default-export': 2,
 		'import/prefer-default-export': 0,
@@ -53,4 +68,4 @@ module.exports = {
 			},
 		],
 	},
-}
+};
