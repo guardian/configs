c1b4a98800db0004b0955d647e24ba9a0c7c2864
Alex Sanders <a@snd.rs>
2021-02-24 11:04:41 +0000

    use eslint-config-prettier v8

--- packages/eslint-config-typescript/index.js
+++ packages/eslint-config-typescript/index.js
@@ -12,7 +12,7 @@ module.exports = {
 		'plugin:@typescript-eslint/recommended',
 		'plugin:@typescript-eslint/recommended-requiring-type-checking',
 		'plugin:import/typescript',
-		'prettier/@typescript-eslint',
+		'prettier',
 	],
 	settings: {
 		'import/extensions': ['.ts', '.tsx'],
