33a26abbad785e24793379bc1f3b26f3759fe7a6
Alex Sanders <a@snd.rs>
2021-02-24 11:11:49 +0000

    plugin:prettier/recommended is all you need

--- packages/eslint-config-typescript/index.js
+++ packages/eslint-config-typescript/index.js
@@ -12,7 +12,6 @@ module.exports = {
 		'plugin:@typescript-eslint/recommended',
 		'plugin:@typescript-eslint/recommended-requiring-type-checking',
 		'plugin:import/typescript',
-		'prettier',
 	],
 	settings: {
 		'import/extensions': ['.ts', '.tsx'],
