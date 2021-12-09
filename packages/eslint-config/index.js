af154e1ddd5d96f0ae67d992de298590040de3b7
Max Duval <max.duval@theguardian.com>
2021-12-09 10:00:46 +0000

    lint files

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -16,8 +16,8 @@ module.exports = {
 	plugins: ['eslint-comments'],
 	rules: {
 		// prevent dangling returns without braces
-		"curly": ["error", "multi-line"],
-		
+		curly: ['error', 'multi-line'],
+
 		// require a `eslint-enable` comment for every `eslint-disable` comment
 		'eslint-comments/disable-enable-pair': [2, { allowWholeFile: true }],
 
